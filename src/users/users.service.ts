6bbd68f4613a039ef4682602d2d078aca9f03f23
D. Nikolov <d.nikolov7654@gmail.com>
2024-04-24 19:11:54 +0300

    update getSelf()

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -14,6 +14,7 @@ import { InvitesService } from 'src/invites/invites.service';
 const saltOrRounds = 15;
 
 export class SelfDto {
+  username: string;
   joinedRooms: Chatroom[];
   ownedRooms: Chatroom[];
   pendingInvites: Invite[];
@@ -53,6 +54,7 @@ export class UsersService {
     }
 
     return {
+      username: username,
       joinedRooms: user.joinedRooms,
       ownedRooms: user.ownedRooms,
       pendingInvites: user.pendingInvites,
